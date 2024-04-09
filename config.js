a532f3774d4de455ff2d759030a07f10c01f4a97
MoreThanBasketball <166460804+MoreThanBasketball@users.noreply.github.com>
2024-04-08 20:20:27 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    Cognito: {
+      userPoolID: 'us-east-1_gldtb99Fn',
+      userPoolClientID: '1vus24ti0343372ovimt6jafsp',
+      region: ''
+    },
+    api: {
+        invokeUrl: 'us-east-1',
+    }
+};
