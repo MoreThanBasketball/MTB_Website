d4dbc431f8dd6d1d687a33bbd33efb735a35f869
More Than Basketball <morethanbasketball29@gmail.com>
2024-04-08 23:57:57 -0500

    test 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var currentIndex = 0;
 
 function prevImage() {
@@ -13,20 +13,4 @@ function nextImage() {
     galleryItems[currentIndex].classList.remove('active');
     currentIndex = (currentIndex + 1) % galleryItems.length;
     galleryItems[currentIndex].classList.add('active');
-=======
-var currentIndex = 0;
-
-function prevImage() {
-    var galleryItems = document.querySelectorAll('.gallery-item');
-    galleryItems[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
-    galleryItems[currentIndex].classList.add('active');
-}
-
-function nextImage() {
-    var galleryItems = document.querySelectorAll('.gallery-item');
-    galleryItems[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % galleryItems.length;
-    galleryItems[currentIndex].classList.add('active');
->>>>>>> 86ff28cbf96ad51b2951dc35fa535c647b5b0f45
 }
